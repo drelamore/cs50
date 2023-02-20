0d0776c23360984a4786df703171705c2d9dd518
U-DESKTOP-N9HI7HH\Μανος <malk@mailinator.com>
2023-02-20 15:22:26 +0200

    fisrt exercise

---
+++ lama_population.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int calc_year(int p);
+int give_pop(void);
+
+int main(void)
+{
+    int p = give_pop();
+	int y = calc_year(p);
+	printf("The population will double in %i years\n", y);
+}
+
+int give_pop(void)
+{
+    int p;
+    do
+	{
+		printf("Give population (bigger or equal to 9): ");  
+	    scanf("%d", &p);
+	}
+    while (p < 9);
+    
+	return p;
+}
+
+int calc_year(int p)
+{
+	int p_o = p; // Original population
+	int i = 0;
+	do
+	{
+		p =  p + (p/3 - p/4); // Every year p/3 are born, p/4 die
+		i++;
+	}
+	while (p < 2*p_o);
+
+	return i;
+}
