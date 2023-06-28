925bb9231b7e6e2e2d8361055678538157e2d0a9
Manos M <inmrnb@gmail.com>
2023-06-28 18:52:40 +0300

    fix whitespace

--- lama_population.c
+++ lama_population.c
@@ -3,32 +3,27 @@
 int calc_year(int p);
 int give_pop(void);
 
-int main(void)
-{
+int main(void) {
     int p = give_pop();
 	int y = calc_year(p);
 	printf("The population will quadruple in %i years\n", y);
 }
 
-int give_pop(void)
-{
+int give_pop(void) {
     int p;
-    do
-	{
-		printf("Give population (bigger or equal to 9): ");  
+    do {
+		printf("Give population (bigger or equal to 9): ");
 	    scanf("%d", &p);
 	}
     while (p < 9);
-    
+
 	return p;
 }
 
-int calc_year(int p)
-{
+int calc_year(int p) {
 	int p_o = p;
 	int i = 0;
-	do
-	{
+	do {
 		p =  p + (p/3 - p/4); // Every year p/3 are born, p/4 die
 		i++;
 	}
